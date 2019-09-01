07af5110733ceb89a446ebb3b778303198059405
Fabian Volkers <fabianvolkers@gmail.com>
2019-09-01 16:53:48 +0200

    added vuetify

---
+++ src/components/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Welcome Back, {{ username }}!</h1>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  props: {
+    username: String
+  }
+}
+</script>
